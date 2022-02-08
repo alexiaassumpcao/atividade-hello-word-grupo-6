92ef1c0a233905d1aa859054f0415bd02548f86e
JonissonGomes <joni_gomes@hotmail.com>
2022-02-08 20:35:41 -0300

    Criando uma página nova de boas vindas

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import logo from './production.svg';
 import './App.css';
 
 function App() {
+
+  const welcome = "Bem vindo";
+  const academy = "</ Globo Academy>"
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p>{welcome}</p>
+        <img src={logo} />
+        <p>{academy}</p>
+        </div>
       </header>
     </div>
   );
